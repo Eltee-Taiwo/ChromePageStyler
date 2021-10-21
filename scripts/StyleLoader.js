5a66d747c9b96f9fd19848c7e7d1a218c84d4014
Eltee <lytaiwo@hotmail.com>
2021-10-20 23:48:06 -0500

    Style is loaded into the dom on page load

--- scripts/StyleLoader.js
+++ scripts/StyleLoader.js
@@ -3,6 +3,30 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ "google.com" : "body {background-color: #3aa757;}" });
+  chrome.storage.sync.set({ "google.com": "body {background-color: #3aa757;}" });
   console.log('Default background color set to %cgreen', `color: #FF0000`);
 });
+
+// When the tab loads, inject setPageBackgroundColor into current page
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+  if (!tab.url.startsWith("chrome")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: setPageBackgroundColor,
+    });
+  }
+})
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  const myRegexp = new RegExp("^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)", "g");
+  var matches = myRegexp.exec(document.domain);
+  var domain = matches[1];
+  console.log(domain);
+  chrome.storage.sync.get(domain, (item) => {
+    var style = document.createElement('style')
+    style.innerHTML = item[domain];
+    document.head.appendChild(style)
+  });
+}
