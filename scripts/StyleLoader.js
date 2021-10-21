587675ee7e1349ac4045142d87eeb24ad4abaa9d
Eltee <lytaiwo@hotmail.com>
2021-10-21 02:16:17 -0500

    Fixed bug where background color kept getting ser to green.

--- scripts/StyleLoader.js
+++ scripts/StyleLoader.js
@@ -1,10 +1,8 @@
 // background.js
 
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ "google.com": "body {background-color: #3aa757;}" });
-  console.log('Default background color set to %cgreen', `color: #FF0000`);
+  // chrome.storage.sync.set({ "google.com": "body {background-color: #3aa757;}" });
+  // console.log('Default background color set to %cgreen', `color: #FF0000`);
 });
 
 // When the tab loads, inject setPageBackgroundColor into current page
